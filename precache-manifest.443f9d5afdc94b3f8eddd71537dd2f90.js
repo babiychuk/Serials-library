a75a010c3e20591c7ba96c8dc7a87f29930e7985
Evgeniy <e.babiychuk.ua@gmail.com>
2019-12-17 19:23:40 +0200

    Updates

---
+++ precache-manifest.443f9d5afdc94b3f8eddd71537dd2f90.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf37a073d66ef3656df6271a359efb1e",
+    "url": "/serials-library/index.html"
+  },
+  {
+    "revision": "0cd796f0f446fc763d10",
+    "url": "/serials-library/static/css/2.c51b6406.chunk.css"
+  },
+  {
+    "revision": "0cd796f0f446fc763d10",
+    "url": "/serials-library/static/js/2.301ee21e.chunk.js"
+  },
+  {
+    "revision": "b2adde24aa49f7441e09",
+    "url": "/serials-library/static/js/main.e9759ee2.chunk.js"
+  },
+  {
+    "revision": "16576ca8896e49ebc90b",
+    "url": "/serials-library/static/js/runtime-main.f5728a12.js"
+  },
+  {
+    "revision": "dfb4c9c2965be4a4c6a49479d170a57b",
+    "url": "/serials-library/static/media/not_found.dfb4c9c2.jpg"
+  },
+  {
+    "revision": "33174a3594504f11d0415f5eb29acff6",
+    "url": "/serials-library/static/media/tv.33174a35.jpg"
+  }
+]);
